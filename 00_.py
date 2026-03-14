5fac8c1bf6bc50519ce761d3a88bf2916f67cc02
anita <anita.bacova@gmail.com>
2026-03-14 23:10:34 +0100

    lesson twenty

---
+++ 00_.py
@@ -0,0 +1,10 @@
+# class BankAccount:
+#
+#     def __init__(self, balance):
+#         self.balance = balance
+#
+#     def withdraw(self, amount):
+#         if self.balance >= amount:
+#             new_balance = self.balance - amount
+#             time.sleep(0.0001)
+#             self.balance = new_balance
